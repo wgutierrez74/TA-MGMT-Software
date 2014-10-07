36af05950bb791297293b8fc9019cb39215e362f
PutmickJ <patrick0392@Patrick-Scanlans-MacBook-Pro.local>
2014-10-07 13:05:18 -0400

    Revert c8e0111..fd23aa9
    This rolls back to commit c8e011115c115ddcbb0e3953450f873258ca33b9.

--- test/app/controllers/users/users.authentication.server.controller.js
+++ test/app/controllers/users/users.authentication.server.controller.js
@@ -16,10 +16,6 @@ exports.signup = function(req, res) {
 	// For security measurement we remove the roles from the req.body object
 	delete req.body.roles;
 
-
-console.log(req.body);
-
-
 	// Init Variables
 	var user = new User(req.body);
 	var message = null;
