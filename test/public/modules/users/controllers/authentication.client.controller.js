3f660128deeb0bf40512bbe5317cfcf2bc836f7c
wgutierrez74 <williamgutierrez74@yahoo.com>
2014-10-14 23:07:44 -0400

    Path change and extra features of edit
    After login redirected to edit profile page

--- test/public/modules/users/controllers/authentication.client.controller.js
+++ test/public/modules/users/controllers/authentication.client.controller.js
@@ -13,7 +13,7 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 				$scope.authentication.user = response;
 
 				// And redirect to the index page
-				$location.path('/');
+				$location.path('/settings/profile');
 			}).error(function(response) {
 				$scope.error = response.message;
 			});
@@ -28,7 +28,7 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 				}
 				else{
 					// And redirect to the index page
-					$location.path('/');
+					$location.path('/settings/profile');
 				}
 			
 				
