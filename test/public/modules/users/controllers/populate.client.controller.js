77db3379794fcbfd817044cba39b16e1033efade
wgutierrez74 <williamgutierrez74@yahoo.com>
2014-10-15 12:39:41 -0400

    Limited View based on type of user

--- test/public/modules/users/controllers/populate.client.controller.js
+++ test/public/modules/users/controllers/populate.client.controller.js
@@ -3,12 +3,14 @@
 angular.module('users').controller('PopulateController', ['$scope', '$http', '$location', 'Authentication',
 	function($scope, $http, $location, Authentication) {
     	$scope.authentication = Authentication;
-    	$scope.applicantsList = [];
-    	$http.get('/populate').success(function(data, status, headers, config){
+    	if($scope.authentication.user.admin){
+            $scope.applicantsList = [];
+    	    $http.get('/populate').success(function(data, status, headers, config){
     		$scope.applicantList = data;
     	//khj
-    	}).error(function(data, status, headers, config){
+    	   }).error(function(data, status, headers, config){
     		$scope.error = status;
-    	});
+    	   });
+        }
 
   }]);
